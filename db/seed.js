84eb274867a22b0b582bdd781b0faa66e92b1764
an nguyen <hello.an.ng@gmail.com>
2017-01-31 16:42:01 -0800

    saves restful kittens

--- db/seed.js
+++ db/seed.js
@@ -18,3 +18,20 @@ Kitten
 .then( () => {
   process.exit() // exit the script
 });
+
+// [
+  // {
+    // "_id": "58910f68c73acbce710bc758",
+    // "name": "Denzel",
+    // "img": "http://placekitten.com/g/200/300",
+    // "__v": 0,
+    // "createdAt": "2017-01-31T22:27:52.178Z"
+  // },
+  // {
+    // "_id": "58910f68c73acbce710bc759",
+    // "name": "Feliz",
+    // "img": "http://placekitten.com/g/200/300",
+    // "__v": 0,
+    // "createdAt": "2017-01-31T22:27:52.184Z"
+  // }
+// ]
--- public/main.js
+++ public/main.js
@@ -1,1 +1,85 @@
 console.log('🐱');
+
+class Kitten {
+  constructor(cat) {
+    this._attributes = cat;
+  }
+  meow() {
+    console.log(this._attributes.name + ' says meow');
+  }
+  get(attr) {
+    console.log(this._attributes[attr]);
+  }
+  set(attr, value) {
+    this._attributes[attr] = value;
+  }
+  save() {
+    $.ajax({
+      method: 'PUT',
+      url: '/kittens/' + this._attributes._id,
+      data: {kitten: this._attributes}
+    })
+    .then(response => {
+      console.log(response);
+    })
+  };
+  remove() {
+    $.ajax({
+      method: 'DELETE',
+      url: '/kittens/' + this._attributes._id
+    })
+    .then(response => {
+      console.log(response);
+    })
+  };
+  static create(kitten) {
+    $.ajax({
+      method: 'POST',
+      url: '/kittens',
+      data: {kitten: kitten}
+    })
+    .then(response => {
+      console.log(response);
+    })
+  };
+  static fetch() {
+    $.ajax({
+      method: 'GET',
+      url: '/kittens',
+    })
+    .then(response => {
+      console.log(response);
+      Kitten.allKittens = [];
+      response.forEach(function(kitten) {
+        var cat = new Kitten(kitten);
+        Kitten.allKittens.push(cat);
+      })
+    })
+  };
+  static all() {
+    if (Kitten.allKittens) {
+      return Kitten.allKittens;
+    } else {
+      return Kitten.allKittens = [];
+    }
+  }
+  static first() {
+    return Kitten.allKittens[0];
+  }
+  static last() {
+    return Kitten.allKittens[Kitten.allKittens.length - 1];
+  }
+  static meow() {
+    Kitten.allKittens.forEach(function(kitten) {
+      kitten.meow();
+    })
+  }
+}
+
+const kitten = new Kitten({name: 'tiny', _id: '58910f68c73acbce710bc758'});
+
+// console.log(kitten._attributes);
+// console.log(kitten);
+// kitten.meow();
+// kitten.get('name');
+
